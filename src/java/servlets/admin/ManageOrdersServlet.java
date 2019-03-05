5913f3b2fb6eb64379f773b921d70e85de0e36a1
Yinghao Li <yinghao.li@edu.sait.ca>
2019-03-05 16:14:35 -0700

    test2

--- src/java/servlets/admin/ManageOrdersServlet.java
+++ src/java/servlets/admin/ManageOrdersServlet.java
@@ -34,7 +34,7 @@ public class ManageOrdersServlet extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
-//            hello
+//            hello---
     {
     }
 }
